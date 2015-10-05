b782a1db075ea356cd53d34346f323fd748c596a
sheldonso <sheldonso@datacom.co.nz>
2015-10-06 09:49:34 +1300

    Initial commit.

---
+++ Hello-World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello-World
+//
+//  Created by Sheldon So on 6/10/15.
+//  Copyright © 2015 Datacom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

f901563c65b324ab6b3624e7b75651f1dfc76623
sheldonso <sheldonso@datacom.co.nz>
2015-10-08 11:35:15 +1300

    - .travis.yml update.  - script update.

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -12,3 +12,15 @@ PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_
 OUTPUTDIR="$PWD/build/Release-iphoneos"
 
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+
+RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
+RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
+
+curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
+-F status="2" \
+-F notify="0" \
+-F notes="$RELEASE_NOTES" \
+-F notes_type="0" \
+-F ipa="@$OUTPUTDIR/$APP_NAME.ipa" \
+-F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
+-H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"

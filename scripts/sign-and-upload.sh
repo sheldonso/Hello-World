b0d2355751c5c096969eec2cf23df99948a6913e
sheldonso <sheldonso@datacom.co.nz>
2015-10-08 16:46:44 +1300

    script update.

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -11,7 +11,10 @@
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
 OUTPUTDIR="$PWD/build/Release-iphoneos"
 
-xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+#xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+#xcrun -sdk iphoneos PackageApplication -v "$PWD/build/Release-iphoneos/Hello-World.app" -o "$PWD/build/Release-iphoneos/Hello-World.ipa" --sign "iPhone Distribution: Datacom (3W2D35BGJW)"
+xcrun -sdk iphoneos PackageApplication -v "$OUTPUTDIR/$APP_NAME.app" -o "$OUTPUTDIR/$APP_NAME.ipa" --sign "$PROVISIONING_PROFILE"
+
 
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"

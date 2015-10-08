c9db6838db6f010013dd6c775e4e4b2119a42aeb
sheldonso <sheldonso@datacom.co.nz>
2015-10-08 14:34:55 +1300

    script update.

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -16,11 +16,5 @@ xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTP
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
 
-curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions \
--F status="2" \
--F notify="0" \
--F notes="$RELEASE_NOTES" \
--F notes_type="0" \
--F ipa="@$OUTPUTDIR/$APP_NAME.ipa" \
--F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM" \
--H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
+curl https://rink.hockeyapp.net/api/2/apps/$HOCKEY_APP_ID/app_versions -F status="2" -F notify="0" -F notes="$RELEASE_NOTES" -F notes_type="0" -F ipa="@$OUTPUTDIR/$APP_NAME.ipa" -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM" -H "X-HockeyAppToken: $HOCKEY_APP_TOKEN"
+
